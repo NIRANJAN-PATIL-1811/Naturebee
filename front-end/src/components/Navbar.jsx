bfb514e4613a8a8e1009861b388700a9f2beb3b6
rosh.rp1811@gmail.com <rosh.rp1811@gmail.com>
2025-03-19 17:35:45 -0400

    Add new changes

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -21,21 +21,21 @@ function Navbar() {
 
   return (
     <>
-      <div className="parent-div flex items-center min-h-[7vh] border-b-0 border-b-cyan-900 shadow-md fixed top-0 left-0 right-0 bg-[white] z-50 w-full">
+      <div className="parent-div flex items-center max-h-[7vh] border-b-0 border-b-cyan-900 shadow-md fixed top-0 left-0 right-0 bg-[white] z-50 w-full">
 
-        <div className="child-div min-h-[5vh] flex items-center ml-[1vw] w-full">
+        <div className="child-div max-h-[5vh] flex items-center ml-[1vw] w-full">
           <div>
             <RxHamburgerMenu onClick={() => changeVal(!open)} className='text-2xl md:hidden ml-[1vw] ' />
           </div>
           <div className="name-div hidden md:flex md:ml-[1vw] md:text-2xl md:font-bold">
             <div className="logo-div">
-              <img className='min-h-[4vh] mr-[1vw]' src="/naturebee.jpeg" alt="" />
+              <img className='max-h-[4vh] mr-[1vw]' src="/naturebee.jpeg" alt="" />
             </div>
             naturebee
           </div>
 
-          <div className="search-div flex justify-center border rounded-sm w-[75%] min-h-[80%] ml-[5%]">
-            <input className='w-[95%] min-h-[100%] outline-none md:w-[98%]' type="text" placeholder="eg. fresh" />
+          <div className="search-div flex justify-center border rounded-sm w-[75%] max-h-[80%] ml-[5%]">
+            <input className='w-[95%] max-h-[100%] outline-none md:w-[98%]' type="text" placeholder="eg. fresh" />
           </div>
 
           <div className="account-div flex">
@@ -49,14 +49,14 @@ function Navbar() {
           {
             open ? 
             <>
-              <div className="min-h-[100vh] w-[50vw] bg-white fixed shadow-2xl">
+              <div className="max-h-[100vh] w-[50vw] bg-white fixed shadow-2xl">
                 <div className="w-70%">
                   
                   <Link to={'/'}>
                     <div onClick={() => changeVal(false)} className="border ml-2  w-[90%] mt-2 rounded-2xl">
                       <div className=" flex items-center ml-2">
                         <SlHome/>
-                        <li className="flex items-center ml-[5%] min-h-10">Home</li>
+                        <li className="flex items-center ml-[5%] max-h-10">Home</li>
                       </div>
                     </div>
                   </Link>
@@ -64,35 +64,35 @@ function Navbar() {
                   <div className="border ml-2  w-[90%] mt-2 rounded-2xl">
                     <div className=" flex items-center ml-2">
                       <VscAccount/>
-                      <li className="flex items-center ml-[5%] min-h-10">Account</li>
+                      <li className="flex items-center ml-[5%] max-h-10">Account</li>
                     </div>
                   </div>
 
                   <div className="border ml-2  w-[90%] mt-2 rounded-2xl">
                     <div className=" flex items-center ml-2">
                       <MdOutlineAccountBalance/>
-                      <li className="flex items-center ml-[5%] min-h-10">Payments</li>
+                      <li className="flex items-center ml-[5%] max-h-10">Payments</li>
                     </div>
                   </div>
 
                   <div className="border ml-2  w-[90%] mt-2 rounded-2xl">
                     <div className=" flex items-center ml-2">
                       <SlBasket/>
-                      <li className="flex items-center ml-[5%] min-h-10">Cart</li>
+                      <li className="flex items-center ml-[5%] max-h-10">Cart</li>
                     </div>
                   </div>
 
                   <div className="border ml-2  w-[90%] mt-2 rounded-2xl">
                     <div className=" flex items-center ml-2">
                       <CiLocationOn/>
-                      <li className="flex items-center ml-[5%] min-h-10">Location</li>
+                      <li className="flex items-center ml-[5%] max-h-10">Location</li>
                     </div>
                   </div>
 
                   <div className="border ml-2  w-[90%] mt-2 rounded-2xl">
                     <div className=" flex items-center ml-2">
                       <TbTruckDelivery/>
-                      <li className="flex items-center ml-[5%] min-h-10">Orders</li>
+                      <li className="flex items-center ml-[5%] max-h-10">Orders</li>
                     </div>
                   </div>
                 </div>
